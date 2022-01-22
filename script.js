9015ea04ff19e9f7677ddb8f8545a8c380d03050
Paul Göckede <pgoeckede@googlemail.com>
2022-01-23 00:27:23 +0100

    added basic dom manipulation and win condition check

--- script.js
+++ script.js
@@ -2,37 +2,108 @@
 
 //player object factory function
 const player = (input) => {
-    const _playerInput = input;
+    const playerInput = input;
 
+    //calls the function to change the array accordingly
     const makeMove = (field) => {
-        gameBoard.changeArray(_playerInput, field);
+        return gameBoard.changeArray(playerInput, field);
     }
 
-    return {_playerInput};
+    return {playerInput, makeMove};
 }
+
 //Gameboard module - keeps track of players move and the game.
 const gameBoard = (() => {
     const _game = ["","","","","","","","","",""];
-    const _playerOne = player("x");
-    const _playerTwo = player("o");
+    let _turns = 0;
     
-    const changeArray = (field) => {
-        // game[input] = field;
-        console.log(field);
+    //checks whether the field selected by each player is empty or not
+    const isFieldEmpty = (field) => {
+        if(_game[field]===""){
+            return true;
+        }
+        return false;
+    }
+
+    //changes the array that keeps track of the playing fields accordingly if the player selection is not an occupied field
+    const changeArray = (input, field) => {
+        if(isFieldEmpty(field)){
+            _game[field] = input;
+            return true;
+        }
+        return false;
     };
-    return {changeArray};
+
+    //logs the playing field for testing purposes
+    const logArray = () => {
+        console.log(_game);
+    }
+
+    //checks if a player has won
+    const checkWinCondition = (input) => {
+        if((_game[0] === input && _game[1] === input && _game[2] === input) ||
+           (_game[3] === input && _game[4] === input && _game[5] === input) ||
+           (_game[6] === input && _game[7] === input && _game[8] === input) ||
+           (_game[0] === input && _game[3] === input && _game[6] === input) ||
+           (_game[1] === input && _game[4] === input && _game[7] === input) ||
+           (_game[2] === input && _game[5] === input && _game[8] === input) ||
+           (_game[0] === input && _game[4] === input && _game[8] === input) ||
+           (_game[2] === input && _game[4] === input && _game[6] === input) ){
+               _turns++;
+               return true;
+           }
+        _turns++;
+        return false;
+    }
+
+    //checks if the game has drawn
+    const checkDraw = () => {
+        if(_turns===9){
+            return true;
+        }
+        return false;
+    }
+
+    return {changeArray, logArray, checkWinCondition, checkDraw};
 })();
 
 //functionality for displaying the current game on the screen
 const displayController = (() => {
-    const _fields = document.querySelectorAll(".field");
+    let _turn = 0; //this keeps track whether player 1 or 2 is currently playing
+
+    const _fields = document.querySelectorAll(".field"); //grabs all 9 fields
     const _fieldsArray = Array.from(_fields);
     _fieldsArray.forEach(function(field){
         field.addEventListener("click", (e) => {
-            gameBoard.makeMove(e.target.dataset.field);
+            if(_turn%2===0){ //if this is true, then its player 1s turn
+                if(playerOne.makeMove(e.target.dataset.field)){ //condition to check if selected field is empty or not
+                    gameBoard.logArray();
+                    _turn++;
+                    field.innerHTML = playerOne.playerInput;
+                    if(gameBoard.checkWinCondition(playerOne.playerInput)){
+                        console.log("Player One Wins!");
+                    } else if(gameBoard.checkDraw()){
+                        console.log("It's a draw!");
+                    }
+                }
+            }else{ //player 2s turn
+                if(playerTwo.makeMove(e.target.dataset.field)){ //condition to check if selected field is empty or not
+                    gameBoard.logArray();
+                    _turn=0;
+                    field.innerHTML = playerTwo.playerInput;
+                    if(gameBoard.checkWinCondition(playerTwo.playerInput)){
+                        console.log("Player Two Wins!");
+                    } else if(gameBoard.checkDraw()){
+                        console.log("It's a draw!");
+                    }
+                }
+            }
         });
     });
 })();
+
+const playerOne = player("x");
+const playerTwo = player("o");
 
 
 /* 
